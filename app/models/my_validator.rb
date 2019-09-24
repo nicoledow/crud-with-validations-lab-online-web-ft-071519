2b751346349150ae0661dc38ca81a07d96dfa200
Nicole Dow <nikki.dow71@gmail.com>
2019-09-23 21:19:21 -0400

    Done.

--- app/models/my_validator.rb
+++ app/models/my_validator.rb
@@ -1,3 +1,24 @@
 class MyValidator < ActiveModel::Validator
 
+    #if a song with the same title and same year exists --> error message
+    #if released = true and released_year == nil --> error message
+    #if release_year > current_year --> error message
+    #otherwise, error message << false
+    def validate(record)
+      if Song.all.detect {|song| song.title == record.title} && Song.all.detect {|song| song.release_year == record.release_year}
+        record.errors[:title] << 'A song with this title for this year already exists'
+      end
+
+      if record.released == true && release_year == nil
+        record.errors[:release_year] << 'Must include release year']
+      end
+
+      if release_year > Time.now.year
+        record.errors[:release_year] << 'Release year cannot be in the future'
+      end
+
+      if record.errors[:title].empty? && record.errors[:release_year].empty?
+        record.errors << false
+      end
+    end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,9 @@
 class Song < ApplicationRecord
+  include ActiveModel::Validations
+
     validates :title, presence: true
 
 
     validates_with MyValidator
+
 end
